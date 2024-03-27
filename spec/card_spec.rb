ed21998f2f7a900825f53ce1591b84c9654858aa
Kaelon Townsend <kaelontownsend@gmail.com>
2024-03-26 23:39:00 -0500

    Added deck shuffle test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,10 +1,20 @@
 require 'card'
+require 'deck'
 
 RSpec.describe Card do
-    let(:card) {Card.new("clubs", "two")}
-    describe "#to_s" do
-        it "correctly represents a single playing card" do
-            expect(card.to_s).to eq("This is the two of clubs")
+    let(:card) {Card.new("clubs", "two")} #create object to run test
+    describe "#to_s" do #test the to_s method
+        it "correctly represents a single playing card" do 
+            expect(card.to_s).to eq("This is the two of clubs") 
+        end
+    end
+end
+
+RSpec.describe Deck do
+    let(:deck) {Deck.new} #create deck object
+    describe "#shuffle" do #testing the shuffle method
+        it "correctly shuffles the deck" do
+            expect(deck.shuffle).not_to eq(deck)
         end
     end
 end
